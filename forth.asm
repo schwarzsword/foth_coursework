9ae0530b48d462a17582ba97a269d9745c3dab0c
schwarzkv <schwarsword@gmail.com>
2019-06-09 15:10:35 +0300

    fixed smt, part 1 can't be added without cycles

--- forth.asm
+++ forth.asm
@@ -1,140 +1,135 @@
-%define pc r15
-%define w  r14
-%define rstack r13
-%define here rbx
+%include "macro.inc"
+%include "lib.inc"
+%include "words.inc"
 
-%include 'lib.inc'
-%include 'macro.asm'
-%include 'dict.asm'
+%define pc r12
+%define w r13
+%define rstack r14
 
 section .data
-    program_stub:       dq 0
-    xt_interpreter:     dq .interpreter
-    .interpreter:       dq interpreter_loop
-    
-    xt_compiler:     	dq .compiler
-    .compiler:       	dq compiler_loop
-    
-    undefined:          db 'no such word', 10, 0
-    underflow:			db 'you need more arguments to execute this', 10, 0
-    compiler_msg		db 'Switch to compiler mode', 10, 0
+last_word: 	dq link
+rsp_b: 		dq 0
+no_word: 	db ' no such word', 10, 0
+here: 		dq dict
+xt_exit: 	dq exit
+program_stub: 	dq 0
+xt_interpreter: dq .interpreter
+.interpreter: 	dq interpreter_loop
 
-section .bss
-	retstack: 			resq 65536 
-	userstack: 			resq 65536
-	dictionary			resq 65536
-	stackHead:  		resq 1
-	ustackHead:  		resq 1
-	state:      		resq 1
-	branch:				resq 1
-	
 section .text
 global _start
 _start:
-	; Инициализация интерпретатора
-	mov [stackHead], rsp
-	mov rstack, retstack + 65536*word_size
-	mov qword[ustackHead], userstack + 65536*word_size
-	mov here, dictionary
-	mov pc, xt_interpreter
-	jmp     next
+	mov rstack, rs + 65536 * 8
+	mov [rsp_b], rsp
+    	mov pc, xt_interpreter
+    	jmp next
 
-; Цикл интерпретатора
 interpreter_loop:
-	mov al, byte[state]
-	test al, al
-	jnz compiler_loop
-	call read_word
-	mov rdi, rax
-	call find_word
-	cmp rax, branch
-	je unknown
+    	cmp qword [state], 0  		; check mode
+    	jne compiler_loop
+
+	xor rax, rax
+	mov qword [word_buffer], rax
+	mov rdi, word_buffer
+	call read_word		    	; word_buffer <- stdin
+	cmp qword [word_buffer], 0x00
+	jz .EOF			            		 
+	call find_word			; try as defined word
+					; rax <- w_word
 	test rax, rax
-	jnz execute
+	jz .parse_number	    	; if !(word in dict) -> try parse as number
+
+	mov rdi, rax
+	call cfa		        ; else rax <- xt_word
+
+	mov [program_stub], rax
+	mov pc, program_stub		; ptr to next ex token = found xt_word
+	jmp next
+.parse_number:
+	mov rdi, word_buffer
 	call parse_int
+
 	test rdx, rdx
-	jz unknown
-	push rax
-	jmp interpreter_loop
+	jz .no_word		        ; if lenght == 0 rip
 
-; Цикл компилятора
-compiler_loop:
-	;mov rdi, compiler_msg
-	;call print_string
-	mov al, byte[state]
-	test al, al
-	jz interpreter_loop
-	
-	call read_word
-	mov rdi, rax
-	call find_word
-	
-	test rax, rax
-	jz .check_number
-	mov rdi, rax
-	call cfa
-	mov dil, byte[rax-1]
-	and dil, 0x01
-	test dil, dil
-	jz .compile ; Прыжок, если нужна компиляция
-	mov w, rax
-	mov [program_stub], rax
-	mov pc, program_stub
-	jmp next
-	.compile:
-		mov [here], rax
-		add here, word_size
-		mov dil, byte[rax-1]
-		and dil, 0x02
-		test dil, dil
-		jnz .branch_write
-	jmp compiler_loop
-	.branch_write:
-		mov byte[branch], 1
-	jmp compiler_loop
-	.check_number:
-		call parse_int
-		test rdx, rdx
-		jz unknown
-		mov cl, byte[branch]
-		test cl, cl
-		jnz .branch
-		mov qword[here], xt_lit
-		add here, word_size
-		mov [here], rax
-		add here, word_size
-	jmp compiler_loop
-	.branch:
-		mov byte[branch], 0
-		mov [here], rax
-		add here, word_size
-	jmp compiler_loop
-
-unknown:
-	mov rdi, undefined
-	call print_string
+	push rax		        ; else push n
 	mov pc, xt_interpreter
-	jmp     next
+	jmp next
+.no_word:
+	call print_no_word
+	mov pc, xt_interpreter
+	jmp next
+.EOF:
+	mov rax, 60
+    	xor rdi, rdi
+    	syscall
 
-execute:
-	mov rdi, rax
-	call cfa
-	mov dil, byte[rax-1]
-	and dil, 0x02
-	test dil, dil
-	jnz unknown
-	mov w, rax
-	mov [program_stub], rax
-	mov pc, program_stub
+
+compiler_loop:
+    	cmp qword [state], 1      	; check mode
+    	jne interpreter_loop
+
+    	mov rdi, word_buffer
+    	call read_word              	; word_buffer <- stdin
+    	call find_word              	; try as defined word 
+					; rax <- w_word
+	test rax, rax
+    	jz .parse_number	        ; if !(word in dict) -> try parse as number
+
+    	mov rdi, rax
+    	call cfa		        ; else rax <- xt_word
+
+    	cmp byte [rax - 1], 1		; check as immediate
+    	je .immediate               
+	
+    	mov rdi, [here]			; else add xt to here
+   	mov [rdi], rax              	
+   	add qword [here], 8
+    	jmp compiler_loop
+.parse_number:
+    	mov rdi, word_buffer
+    	call parse_int
+
+    	test rdx, rdx
+    	jz .no_word		        ; if lenght == 0 rip
+
+	mov rdi, [here]
+    	cmp qword [rdi - 8], xt_branch	; check if prev == branch/branch0
+	je .branch
+	cmp qword [rdi - 8], xt_branch0
+	je .branch
+
+    	mov rdi, [here]			; else add xt_lit
+    	mov qword [rdi], xt_lit     	
+    	add qword [here], 8
+.branch:
+	mov rdi, [here]			; add num
+	mov [rdi], rax
+	add qword [here], 8
+	jmp compiler_loop
+.immediate:
+    	mov [program_stub], rax
+    	mov pc, program_stub
+    	jmp next
+.no_word:
+	call print_no_word
+    	mov pc, xt_interpreter
 	jmp next
 
-next: 
-	mov w, pc
-	add pc, 8
-	mov w, [w]
-	jmp [w]
-	
-close:
-	mov rax, 60
-	xor rdi, rdi
-	syscall
+next:
+    	mov w, pc		; ptr to current xt_word
+    	add pc, 8		; set pc as ptr to next xt (xt_interpreter / (xt_word or xt_exit) for colon)
+    	mov w, [w]		; ptr to word_impl or docol
+    	jmp [w]			; jmp to word_impl or docol
+
+docol:
+	sub rstack, 8		
+	mov [rstack], pc	; save ptr to next xt
+	add w, 8	    	
+	mov pc, w	    	
+	jmp next	    	
+
+exit:
+	mov pc, [rstack]	; restore ptr to next xt
+	add rstack, 8		
+	jmp next	
